ff2749f1ca59e7e6cad6aff7eaa3002aeb8ed2a6
amSuperProgrammer <118238515+amSuperProgrammer@users.noreply.github.com>
2023-06-27 15:10:54 +0600

    rework people

--- Assets/Scripts/Library/LibraryPlayer.cs
+++ Assets/Scripts/Library/LibraryPlayer.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class LibraryPlayer : MonoBehaviour
 {
-    public  int speed;
+    public int speed;
+    public int sprint;
+    int playerSpeed;
     float horizontalInput;
     float verticalInput;
     [SerializeField] Animator animator;
@@ -12,10 +14,12 @@ public class LibraryPlayer : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         animator.SetFloat("Move", Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
+        animator.SetBool("Sprint", Input.GetKey(KeyCode.LeftShift));
+        playerSpeed = Input.GetKey(KeyCode.LeftShift) ? sprint : speed;
     }
 
     private void FixedUpdate()
     {
-        transform.Translate(new Vector3(horizontalInput, 0, verticalInput) * Time.deltaTime * speed);
+        transform.Translate(new Vector3(horizontalInput, 0, verticalInput) * Time.deltaTime * playerSpeed);
     }
 }
