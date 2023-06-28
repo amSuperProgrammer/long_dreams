c0eab071547a5f57f0d0c9dd6423b7f4d5840954
DoubleMaster <130893087+DDoubleMaster@users.noreply.github.com>
2023-06-28 18:32:43 +0600

    Jump

--- Assets/Scripts/Library/LibraryPlayer.cs
+++ Assets/Scripts/Library/LibraryPlayer.cs
@@ -4,10 +4,18 @@ public class LibraryPlayer : MonoBehaviour
 {
     public int speed;
     public int sprint;
+    public int jump;
     int playerSpeed;
     float horizontalInput;
     float verticalInput;
     [SerializeField] Animator animator;
+    [SerializeField] LayerMask isGroundedLayer;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
@@ -16,6 +24,11 @@ public class LibraryPlayer : MonoBehaviour
         animator.SetFloat("Move", Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
         animator.SetBool("Sprint", Input.GetKey(KeyCode.LeftShift));
         playerSpeed = Input.GetKey(KeyCode.LeftShift) ? sprint : speed;
+
+        Collider[] isGrounded = Physics.OverlapBox(transform.position, new Vector3(0.8f, 0.2f, 0.8f), Quaternion.Euler(0, 0, 0), isGroundedLayer);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded.Length > 0)
+            rb.velocity = Vector3.up * jump;
     }
 
     private void FixedUpdate()
