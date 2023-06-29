27fc325290636ac1170d10974a9f0307f161f9f9
amSuperProgrammer <118238515+amSuperProgrammer@users.noreply.github.com>
2023-06-29 17:02:16 +0600

    jump

--- Assets/Scripts/Library/LibraryPlayer.cs
+++ Assets/Scripts/Library/LibraryPlayer.cs
@@ -7,7 +7,6 @@ public class LibraryPlayer : MonoBehaviour
     public int jump;
     int playerSpeed;
     float horizontalInput;
-    float verticalInput;
     [SerializeField] Animator animator;
     [SerializeField] LayerMask isGroundedLayer;
     Rigidbody rb;
@@ -19,13 +18,14 @@ public class LibraryPlayer : MonoBehaviour
 
     private void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-        animator.SetFloat("Move", Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
-        animator.SetBool("Sprint", Input.GetKey(KeyCode.LeftShift));
-        playerSpeed = Input.GetKey(KeyCode.LeftShift) ? sprint : speed;
-
         Collider[] isGrounded = Physics.OverlapBox(transform.position, new Vector3(0.8f, 0.2f, 0.8f), Quaternion.Euler(0, 0, 0), isGroundedLayer);
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        animator.SetFloat("Move", Mathf.Abs(horizontalInput));
+        animator.SetBool("Sprint", Input.GetKey(KeyCode.LeftShift));
+        animator.SetBool("Jump", isGrounded.Length == 0);
+
+        playerSpeed = Input.GetKey(KeyCode.LeftShift) ? sprint : speed;
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded.Length > 0)
             rb.velocity = Vector3.up * jump;
@@ -33,6 +33,6 @@ public class LibraryPlayer : MonoBehaviour
 
     private void FixedUpdate()
     {
-        transform.Translate(new Vector3(horizontalInput, 0, verticalInput) * Time.deltaTime * playerSpeed);
+        transform.Translate(new Vector3(horizontalInput, 0, 0) * Time.deltaTime * playerSpeed);
     }
 }
--- Assets/Scripts/Library/PlayerSkin.cs
+++ Assets/Scripts/Library/PlayerSkin.cs
@@ -3,16 +3,14 @@ using UnityEngine;
 public class PlayerSkin : MonoBehaviour
 {
     float horizontalInput;
-    float verticalInput;
     float direction;
 
     private void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
 
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
-            direction = Mathf.Atan2(horizontalInput, verticalInput) * Mathf.Rad2Deg;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+            direction = Mathf.Atan2(horizontalInput, 0) * Mathf.Rad2Deg;
 
         transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, direction, 0), 360 * Time.deltaTime);
     }
