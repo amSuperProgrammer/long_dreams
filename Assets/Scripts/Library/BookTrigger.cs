ce983eea3c5d3f314512d1f4f6115604f338e6f7
amSuperProgrammer <118238515+amSuperProgrammer@users.noreply.github.com>
2023-07-01 17:03:26 +0600

    fast commit

--- Assets/Scripts/Library/BookTrigger.cs
+++ Assets/Scripts/Library/BookTrigger.cs
@@ -1,10 +1,11 @@
+using System.Collections;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class BookTrigger : MonoBehaviour
 {
     [SerializeField] int bookNum;
     [SerializeField] GameObject buttonSpritePrefab;
+    [SerializeField] Vector3 spritePos;
     GameObject buttonSprite;
 
     private void OnTriggerEnter(Collider collision)
@@ -12,17 +13,39 @@ public class BookTrigger : MonoBehaviour
         if (collision.gameObject.name == "Player")
         {
             buttonSprite = Instantiate(buttonSpritePrefab);
-            buttonSprite.transform.position = transform.position + new Vector3(0, 1.25f, -5.25f);
-            if (Input.GetKey(KeyCode.E))
-                SceneManager.LoadScene(bookNum);
+            buttonSprite.transform.position = transform.position + spritePos;
         }
     }
 
     private void OnTriggerStay(Collider collision)
     {
         if (collision.gameObject.name == "Player" && Input.GetKey(KeyCode.E))
-            SceneManager.LoadScene(bookNum);
+        {
+            Destroy(buttonSprite);
+            StartCoroutine(takeBook(collision.gameObject));
+        }
     }
+
+    IEnumerator takeBook(GameObject player)
+    {
+        LibraryPlayer libraryPlayer = player.GetComponent<LibraryPlayer>();
+        PlayerSkin playerSkin = player.GetComponentInChildren<PlayerSkin>();
+        libraryPlayer.enabled = false;
+        playerSkin.enabled = false;
+        player.transform.position = new Vector3(transform.position.x, player.transform.position.y, player.transform.position.z);
+        do
+        {
+            player.transform.rotation = Quaternion.RotateTowards(player.transform.rotation, Quaternion.Euler(Vector3.zero), 60);
+        } while (player.transform.rotation == Quaternion.Euler(Vector3.zero));
+
+        yield return new WaitForSeconds(1f);
+
+        do
+        {
+            player.transform.position = Vector3.MoveTowards(player.transform.position, new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z), 5);
+        } while (player.transform.position == new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z));
+    }
+
 
     private void OnTriggerExit(Collider collision)
     {
--- Assets/Scripts/Library/CameraMove.cs
+++ Assets/Scripts/Library/CameraMove.cs
@@ -3,17 +3,19 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    [SerializeField]List<GameObject> track;
-    [SerializeField]GameObject player;
+    [SerializeField] List<GameObject> track;
+    [SerializeField] GameObject player;
+    [SerializeField] float cameraSpeed;
 
     private void Update()
     {
         float cameraMoveX = player.transform.position.x;
+
         if (track[0].transform.position.x > player.transform.position.x)
             cameraMoveX = track[0].transform.position.x;
         else if (track[^1].transform.position.x < player.transform.position.x)
             cameraMoveX = track[^1].transform.position.x;
 
-        transform.position = new Vector3(cameraMoveX, transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, new Vector3(cameraMoveX, transform.position.y, transform.position.z), cameraSpeed);
     }
 }
