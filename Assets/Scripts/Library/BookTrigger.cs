dd27c33581dd61ff23429c277e6b50ebc6d63494
amSuperProgrammer <118238515+amSuperProgrammer@users.noreply.github.com>
2023-07-03 16:55:18 +0600

    cutscene and sit

--- Assets/Scripts/Library/BookTrigger.cs
+++ Assets/Scripts/Library/BookTrigger.cs
@@ -1,11 +1,12 @@
-using System.Collections;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class BookTrigger : MonoBehaviour
 {
     [SerializeField] int bookNum;
     [SerializeField] GameObject buttonSpritePrefab;
     [SerializeField] Vector3 spritePos;
+    [SerializeField] PlayableDirector director;
     GameObject buttonSprite;
 
     private void OnTriggerEnter(Collider collision)
@@ -22,28 +23,8 @@ public class BookTrigger : MonoBehaviour
         if (collision.gameObject.name == "Player" && Input.GetKey(KeyCode.E))
         {
             Destroy(buttonSprite);
-            StartCoroutine(takeBook(collision.gameObject));
+            director.Play();
         }
-    }
-
-    IEnumerator takeBook(GameObject player)
-    {
-        LibraryPlayer libraryPlayer = player.GetComponent<LibraryPlayer>();
-        PlayerSkin playerSkin = player.GetComponentInChildren<PlayerSkin>();
-        libraryPlayer.enabled = false;
-        playerSkin.enabled = false;
-        player.transform.position = new Vector3(transform.position.x, player.transform.position.y, player.transform.position.z);
-        do
-        {
-            player.transform.rotation = Quaternion.RotateTowards(player.transform.rotation, Quaternion.Euler(Vector3.zero), 60);
-        } while (player.transform.rotation == Quaternion.Euler(Vector3.zero));
-
-        yield return new WaitForSeconds(1f);
-
-        do
-        {
-            player.transform.position = Vector3.MoveTowards(player.transform.position, new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z), 5);
-        } while (player.transform.position == new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z));
     }
 
 
