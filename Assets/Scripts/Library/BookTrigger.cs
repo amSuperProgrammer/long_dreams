8f36164245320d81bd14e4624e979c079e44b77a
DoubleMaster <130893087+DDoubleMaster@users.noreply.github.com>
2023-07-05 12:56:14 +0600

    complete demo library

--- Assets/Scripts/Library/BookTrigger.cs
+++ Assets/Scripts/Library/BookTrigger.cs
@@ -9,23 +9,30 @@ public class BookTrigger : MonoBehaviour
     [SerializeField] Vector3 spritePos;
     [SerializeField] TimelineAsset timelineAsset;
     [SerializeField] PlayableDirector director;
+    [SerializeField] float initialTime;
     GameObject buttonSprite;
 
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.name == "Player")
         {
-            buttonSprite = Instantiate(buttonSpritePrefab);
-            buttonSprite.transform.position = transform.position + spritePos;
+
         }
     }
 
     private void OnTriggerStay(Collider collision)
     {
-        if (collision.gameObject.name == "Player" && Input.GetKey(KeyCode.E))
+        if(collision.gameObject.name == "Player" && director.state == PlayState.Paused && buttonSprite == null)
+        {
+            buttonSprite = Instantiate(buttonSpritePrefab);
+            buttonSprite.transform.position = transform.position + spritePos;
+        }
+
+        if (collision.gameObject.name == "Player" && Input.GetKey(KeyCode.E) && director.state == PlayState.Paused)
         {
             Destroy(buttonSprite);
             director.playableAsset = timelineAsset;
+            director.initialTime = initialTime;
             director.Play();
         }
     }
