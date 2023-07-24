f81fd6adda05ce9231691e3350756da94a4ab526
amSuperProgrammer <118238515+amSuperProgrammer@users.noreply.github.com>
2023-07-24 15:24:34 +0600

    camera move

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -1,14 +1,25 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
+    [SerializeField] List<GameObject> track;
     [SerializeField] GameObject player;
     [SerializeField] float cameraSpeed;
+    float cameraMoveX;
 
     private void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, new Vector3(player.transform.position.x, transform.position.y, transform.position.z), cameraSpeed);
+        cameraMoveX = player.transform.position.x;
+
+        if (track[0].transform.position.x > player.transform.position.x)
+            cameraMoveX = track[0].transform.position.x;
+        else if (track[^1].transform.position.x < player.transform.position.x)
+            cameraMoveX = track[^1].transform.position.x;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(cameraMoveX, transform.position.y, transform.position.z), cameraSpeed);
     }
 }
