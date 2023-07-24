d189a8134696a00105c43ccb9fa53fc12cd8e012
DoubleMaster <130893087+DDoubleMaster@users.noreply.github.com>
2023-07-25 01:10:18 +0600

    update location graphics

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -1,9 +1,7 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    [SerializeField] List<GameObject> track;
     [SerializeField] GameObject player;
     [SerializeField] float cameraSpeed;
     float cameraMoveX;
@@ -11,11 +9,6 @@ public class CameraMove : MonoBehaviour
     private void Update()
     {
         cameraMoveX = player.transform.position.x;
-
-        if (track[0].transform.position.x > player.transform.position.x)
-            cameraMoveX = track[0].transform.position.x;
-        else if (track[^1].transform.position.x < player.transform.position.x)
-            cameraMoveX = track[^1].transform.position.x;
     }
 
     private void FixedUpdate()
